e2ab475df9553f7b2b1ec6c7753cc05a76d5768a
uruam <uruam@pm.me>
2024-03-28 17:00:37 +0200

    Add info text

--- client/src/js/info/info-loader.js
+++ client/src/js/info/info-loader.js
@@ -2,8 +2,20 @@ const infoLoader = () => {
   const someText = document.getElementById("someText");
   someText.innerText = "INFO";
 
+  const text = [
+    `Set <mark>\\cg_trackConsent 1</mark> if you want your nickname to be shown in the stats. Apart from your nickname, no unique identifiers are collected by the website. All data is deleted after 30 days.`,
+    `If your nickname is in the stats and you want it to be removed, simply set <mark>\\cg_trackConsent 0</mark>, and thereafter the nickname will disappear within 30 days.`,
+  ];
+
   const contentContainer = document.getElementById("contentContainer");
-  contentContainer.innerText = "...";
+  const infoWrapperDiv = document.createElement("div");
+  infoWrapperDiv.id = "infoWrapper";
+  infoWrapperDiv.innerHTML = `
+  <p>${text[0]}</p>
+  <p>${text[1]}</p>
+  `;
+
+  contentContainer.appendChild(infoWrapperDiv);
 };
 
 export default infoLoader;
