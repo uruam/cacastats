01f50fb57b2d616f811446c447d0a0e2105c85ab
uruam <uruam@pm.me>
2024-03-28 15:56:30 +0200

    Refactor request-handler.js

--- server/request-handler.js
+++ server/request-handler.js
@@ -15,6 +15,45 @@ const getContentType = (parsedUrl) => {
   return null;
 };
 
+const parseOaqueryToJSON = (output) => {
+  const lines = output.trim().split("\n");
+  const serverInfo = {};
+  const players = [];
+
+  // Parse the first line to extract IP and server name
+  const [ipAndServerName] = lines.shift().split(" Players:");
+  const [ip, serverName] = ipAndServerName.split(" ");
+  serverInfo.ip = ip;
+  serverInfo.serverName = serverName.trim();
+
+  // Parse the rest of the lines
+  lines.forEach((line) => {
+    if (line.includes(":")) {
+      const [key, value] = line.split(":").map((str) => str.trim());
+      if (key === "Players") {
+        serverInfo.numberOfPlayers = value;
+      } else {
+        serverInfo[key.toLowerCase()] = value;
+      }
+    } else if (line.trim() !== "") {
+      // Extract player information
+      const match = line.trim().match(/^(\d+)\s+(\d+ms)\s+(.*)$/);
+      if (match) {
+        const [, score, ping, name] = match;
+        const player = {
+          name,
+          ping,
+          score,
+        };
+        players.push(player);
+      }
+    }
+  });
+  serverInfo.players = players;
+
+  return serverInfo;
+};
+
 const serveStaticFile = (parsedUrl, contentType, res) => {
   const pathName = parsedUrl.pathname;
   let file = `${dirname}/../client/dist${pathName}`;
@@ -73,6 +112,7 @@ const serveDataFile = (res, queryString) => {
         res.end("Internal Server Error");
       }
     }
+
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(data);
   } catch (error) {
@@ -91,45 +131,6 @@ const serveDataFile = (res, queryString) => {
 };
 
 const serveOaquery = (res) => {
-  function parseOutputToJSON(output) {
-    const lines = output.trim().split("\n");
-    const serverInfo = {};
-    const players = [];
-
-    // Parse the first line to extract IP and server name
-    const [ipAndServerName] = lines.shift().split(" Players:");
-    const [ip, serverName] = ipAndServerName.split(" ");
-    serverInfo.ip = ip;
-    serverInfo.serverName = serverName.trim();
-
-    // Parse the rest of the lines
-    lines.forEach((line) => {
-      if (line.includes(":")) {
-        const [key, value] = line.split(":").map((str) => str.trim());
-        if (key === "Players") {
-          serverInfo.numberOfPlayers = value;
-        } else {
-          serverInfo[key.toLowerCase()] = value;
-        }
-      } else if (line.trim() !== "") {
-        // Extract player information
-        const match = line.trim().match(/^(\d+)\s+(\d+ms)\s+(.*)$/);
-        if (match) {
-          const [, playerScore, ping, playerName] = match;
-          const player = {
-            name: playerName,
-            ping,
-            score: playerScore,
-          };
-          players.push(player);
-        }
-      }
-    });
-
-    serverInfo.players = players;
-    return serverInfo;
-  }
-
   const pythonProcess = spawn("python3", [
     "./oaquery.py",
     "96.126.107.177:27200",
@@ -149,12 +150,13 @@ const serveOaquery = (res) => {
 
   pythonProcess.on("close", (code) => {
     if (code === 0 && responseData.trim() !== "") {
-      const serverInfo = parseOutputToJSON(responseData);
+      const serverInfo = parseOaqueryToJSON(responseData);
+
       res.writeHead(200, { "Content-Type": "application/json" });
       res.end(JSON.stringify(serverInfo));
     } else {
       res.writeHead(500, { "Content-Type": "text/plain" });
-      res.end("Error: Python script execution failed or produced no output.");
+      res.end("Error: Python script execution failed or produced no output");
     }
   });
 };
