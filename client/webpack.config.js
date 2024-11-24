41c1a13a889a8b1a29bbd6480474de33fb5b204e
uruam <uruam@pm.me>
2024-11-24 07:54:03 +0200

    Add copy-webpack-plugin and update favicon links

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,6 +2,7 @@ const TerserPlugin = require("terser-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const ESLintPlugin = require("eslint-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require("path");
 
 module.exports = {
@@ -17,6 +18,12 @@ module.exports = {
     }),
     new MiniCssExtractPlugin(),
     new ESLintPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "src/assets/favicon.ico", to: "favicon.ico" },
+        { from: "src/assets/apple-touch-icon.png", to: "apple-touch-icon.png" },
+      ],
+    }),
   ],
   module: {
     rules: [
@@ -31,7 +38,7 @@ module.exports = {
         use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
         exclude: /node_modules/,
         type: "asset/resource",
       },
